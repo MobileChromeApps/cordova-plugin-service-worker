1c45a2feeae0495ddb86f0038f60b5b15a825d19
Max Woghiren <max.woghiren@gmail.com>
2015-02-09 14:56:12 -0500

    Added cache javascript file.

---
+++ www/sw_assets/cache.js
@@ -0,0 +1,13 @@
+CacheStorage = function() {
+
+};
+
+// This function returns a promise for a response.
+CacheStorage.prototype.match = function(request, options) {
+  return new Promise(function(resolve, reject) {
+    match(request, options, resolve, reject);
+  });
+};
+
+var caches = new CacheStorage();
+
